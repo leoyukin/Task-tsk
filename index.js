180ba510bcee0a5d37b1f9c9378d2eb1b4c7805a
leoyukin <anakinyuku@gmail.com>
2026-05-01 21:51:24 -0400

    Added function to edit and delete

--- index.js
+++ index.js
@@ -1,13 +1,103 @@
 const addBtn = document.querySelector(".addBtn");
-const cardscont = document.querySelector(".container-cards");
-const td = document.querySelector(".td");
+let cardscont = document.querySelector(".container-cards");
+let todo = document.querySelectorAll(".td");
+let tdo = document.querySelectorAll(".td");
 const inputBtn = document.querySelector(".inpBtns");
+const cardtitle = document.querySelector(".card-title")
+const edit = document.querySelectorAll(".edit");
+const dele = document.querySelectorAll(".delete")
+let proptog = true;
 
+todo.forEach(td => {
+    td.addEventListener("click", (event) => {
+        const activecard = document.querySelector(".active");
+        const currentcard = event.currentTarget;
+
+        if (currentcard && !activecard && proptog === true) {
+            td.classList.add("active");
+            edit.forEach(edi => {
+                edi.addEventListener("click", (e) => {
+                    e.stopPropagation();
+                    editTask()
+                });
+            });
+            dele.forEach(del => {
+                del.addEventListener("click", function () {
+                    deleteTask();
+                });
+            });
+        }
+    });
+})
+
+todo.forEach(td => {
+    td.addEventListener("dblclick", (event) => {
+        const activecard = document.querySelector(".active");
+        const currentcard = event.currentTarget;
+        const input = td.querySelector(".inptext");
+
+        if (currentcard && activecard && proptog === true && input.value !== "") {
+            td.classList.remove("active");
+        }
+    })
+})
 
 addBtn.addEventListener("click", function () {
     showinput();
 });
 
+dele.forEach(del => {
+    del.addEventListener("click", function () {
+        deleteTask();
+    })
+})
+
+function deleteTask() {
+    const remove = document.querySelector(".active");
+    cardscont.removeChild(remove);
+    return;
+}
+
+function editTask() {
+    const card = document.querySelector(".active")
+    const text = card.querySelector(".card-title");
+    const input = card.querySelector(".inptext");
+    const emptyinput = " ";
+
+    input.value = text.textContent;
+    text.style.display = 'none';
+    input.style.display = 'block';
+    input.focus;
+
+    if (input.value.trim() !== "" && input.value.trim() !== " ") {
+        input.addEventListener("keydown", (e) => {
+            if (e.key === 'Enter' && input.value.trim() !== "") {
+
+                text.textContent = input.value;
+
+                input.style.display = 'none';
+                text.style.display = 'block';
+            }
+
+
+        });
+    }
+    else if (input.value.trim() !== "" && input.value.trim() !== emptyinput) {
+        input.addEventListener("blur", () => {
+
+            text.textContent = input.value;
+
+            input.style.display = 'none';
+            text.style.display = 'block';
+        });
+    }
+    else {
+        showToast('Please enter a task!');
+        input.focus;
+        return tdo = document.querySelectorAll(".td");
+    }
+
+}
 
 function showinput() {
     addBtn.style.display = 'none';
@@ -49,10 +139,6 @@ function showinput() {
         cancelCard(inputBox);
     });
 
-    //inputBox.querySelector(".unknown").addEventListener("click", function () {
-    //    showBtn(inputBox);
-    //});
-
     document.getElementById("ntask").addEventListener("keydown", function (e) {
         const prior = getprior();
         if (e.key === 'Enter') {
@@ -79,14 +165,23 @@ function createCard(inputBox, prior) {
     }
     else {
         const card = document.createElement('div');
-        card.classList.add('td');
+        card.classList.add('card', 'td');
         card.innerHTML = `
-    <div class="card td">
-        <p class="card-title">${task}</p>
-        <span class="priority ${pri}">${prior}</span>
-    </div>
+                        <div class="card td">
+                    <div class="toptitle">
+                        <p class="card-title">${task}</p>
+                        <input type="text" class="inptext" />
+                        <button class="prop edit"><i class="fa-solid fa-pen-to-square"></i></button>
+                    </div>
+                    <div class="divi"></div>
+                    <div class="bottitle">
+                        <span class="priority ${pri}">${prior}</span>
+                        <button class="prop delete"><i class="fa-solid fa-trash"></i></button>
+                    </div>
+                </div>
     `;
 
+        cardscont.appendChild(card);
         cardscont.insertBefore(card, inputBox);
 
         inputBox.remove();
